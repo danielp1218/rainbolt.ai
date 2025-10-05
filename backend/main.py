a66db20a6fc3031454b70e3593122af0a621918d
Daniel Pu <danielpu2007@gmail.com>
2025-10-05 05:52:23 -0400

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://rainboltai.gay", "https://www.rainbolt.ai"],  # Adjust as needed
     allow_credentials=True, 
     allow_methods=["*"],
     allow_headers=["*"]
